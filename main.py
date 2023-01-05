c80181c2d7d3e89c86beb353ead2acaa70dfa6e4
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-01-05 18:34:10 +0700

    Update 2

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import datetime
 import json
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, send_from_directory
 import requests
 
-
 app = Flask(__name__)
+# app.run(debug=True)
 
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=6000, debug=True)
 
-TASKS_FILE = "./config.json"
-
+PROJECTS_FILE = "./projects.json"
+CONFIG_FILE = "./config.json"
 
 def getConfug():
-    with open(TASKS_FILE, "r") as infile:
+    with open(CONFIG_FILE, "r") as infile:
         return json.load(infile)
 
+def getProjects():
+    with open(PROJECTS_FILE, "r") as infile:
+        return json.load(infile)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', config=getConfug())
+    return render_template('index.html', config = getConfug(), projects = getProjects())
+
+@app.route('/mainstyle', methods=['GET'])
+def mainstyle():
+    return send_from_directory('styles', 'mainstyle.css')
+
+@app.route('/smallstyle', methods=['GET'])
+def smallstyle():
+    return send_from_directory('styles', 'smallstyle.css')
+
+@app.route('/<section>', methods=['GET'])
+def vindigio(section):
+    return render_template('project.html', project = getProjects().get(section))
+
+@app.route('/static/<path:path>')
+def send_report(path):
+    return send_from_directory('static', path)
